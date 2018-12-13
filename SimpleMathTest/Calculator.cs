c0067c9c33c1738faf61bb94270e73d0d07feaa0
Josh W <joshwidzer@gmail.com>
2018-12-12 20:17:11 -0500

    F   add calculator class that uses dependency injection

---
+++ SimpleMathTest/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMathTest
+{
+    class Calculator
+    {
+        private IAdder adder;
+
+        public Calculator(IAdder adder)
+        {
+            this.adder = adder;
+        }
+
+        public int AddTwoNumbers(int first, int second)
+        {
+            return adder.Add(first, second);
+        }
+    }
+}
