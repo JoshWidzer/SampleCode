192f64b2d95b0222a1b0c1ec1488d089e747448c
Josh W <joshwidzer@gmail.com>
2018-12-12 20:11:26 -0500

    F   Class that adds two numbers and two simple unit tests

---
+++ SimpleMathTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleMathTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleMathTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("213f5656-b929-48d9-9ee9-4bcc9766c6e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleMathTest/SimpleMathTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleMathTest
+{
+    [TestClass]
+    public class SimpleMathTest
+    {
+        [TestMethod]
+        public void OneAndOneIsTwo()
+        {
+            var math = new SimpleMath();
+            var result = math.Add(1, 1);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TwoAndTwoIsNotThree()
+        {
+            var math = new SimpleMath();
+            var result = math.Add(2, 2);
+            Assert.AreNotEqual(3, result);
+        }
+    }
+}
